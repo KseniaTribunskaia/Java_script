091f0d6b277ecf9522c29fc506b1e9cdf9240934
tribuko <tribuko@gmail.com>
2020-11-12 16:19:44 +0300

    js 1 and 3
    js 1 and 3

---
+++ JavaScript/javascript.js
@@ -0,0 +1,16 @@
+'use strict';
+ // 1
+let height = 10;
+let width = 45;
+let long = 30;
+let S = 2*(long*height+height*width+width*long);
+console.log(S);
+// 2
+
+// 3
+let a = 4;
+let b = 7;
+a = a+1;
+b = b+1
+console.log(a);
+console.log(b);
